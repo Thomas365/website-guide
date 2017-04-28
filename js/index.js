dec4be392c7b1167c492c46eec92161cd32eccc2
丁芳军 <fangjund365@163.com>
2017-04-28 09:17:21 +0800

    更新：增加 “动态添加 js 插件功能”

--- js/index.js
+++ js/index.js
@@ -2,4 +2,17 @@ $(function(){
     $.get('./data/website.json',function(result){        
         $(".website-classify").html(template('website-template',result['website-classify']))
     })
+
+    $('.btnAdd').on('click',function(){
+        var url = $('.url_input').val();
+        if($('script[src="'+url+'"]').length){
+            alert('已经添加，不要重复添加')
+        }else{
+            var id = $('script').length;
+            $('body').append('<script id=add'+id+' src='+url+'></script>');
+            $('.libList').append('<li>'+url+'</li>');
+            $('.url_input').val('');
+        }        
+        
+    })
 })
